39f2f213a9c25c1b3c02616dd95d0cc6d761dd6d
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-14 11:02:32 +0700

    install react router dom and adding route for home and empty page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import NotesContextProvider from './context/NotesContext';
 import PanelContextProvider from './context/PanelContext';
 
 import Home from './pages/Home';
+import Empty from './pages/NotFound';
 
 function App() {
   return (
     <NotesContextProvider>
       <PanelContextProvider>
-        <Home/>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="*" element={ <Empty/> }/>
+        </Routes>
       </PanelContextProvider>
     </NotesContextProvider>
   );
