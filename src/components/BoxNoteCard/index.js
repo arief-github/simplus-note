3c48fb5f43ed62d064ac40cfe1333c1d35d8cf99
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-16 14:21:03 +0700

    fix layout masonry column

--- src/components/BoxNoteCard/index.js
+++ src/components/BoxNoteCard/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import Masonry from 'react-masonry-css';
 
 import NoteCard from '../NoteCard';
+
 import styles from './index.module.css';
 
-// set breakpoint column
 const breakpointColumnsObj = {
-	default: 3,
-	1024: 2,
-	640: 1,
-} 
+  default: 3,
+  1024: 2,
+  640: 1,
+};
 
-function BoxNoteCard( { notes } ) {
-	return (
-		<Masonry
-		  breakpointCols={ breakpointColumnsObj }
-		  className={ styles.masonryGrid }
-		  columnClassName={ styles.masonryGridColumn }
-		>
-			{ notes.map((note) => {
-				return <NoteCard key={note.id} note={note} />
-			}) }
-		</Masonry>
-	)
+function BoxNoteCard({ notes }) {
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className={styles.masonryGrid}
+      columnClassName={styles.masonryColumn}
+    >
+      {notes.map((note) => {
+        return <NoteCard key={note.id} note={note} />;
+      })}
+    </Masonry>
+  );
 }
 
 export default React.memo(BoxNoteCard);
