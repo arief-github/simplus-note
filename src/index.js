e0fe01ec5e8ec9ff743c088273361140e7747e53
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-17 16:10:43 +0700

    Install PWA and Service Worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
 
 import './styles/global.css';
 import App from './App';
@@ -19,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+registerServiceWorker();
